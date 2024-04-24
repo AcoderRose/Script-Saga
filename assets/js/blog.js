ac7e50eaa8444fcd0b939577bb2298a958f37ac7
AcoderRose <addyro96@att.net>
2024-04-23 22:21:22 -0700

    Worked with tutor on file organization and corrected pathways/links, added a working form.js and blog.js, added a rough draft of blog.html, I still need to work on light/dark effect, and add working logic.js.

---
+++ assets/js/blog.js
@@ -0,0 +1,36 @@
+const blogList = document.querySelector("#blog-list");
+let blogs = [];
+
+function renderBlogs() {
+  for (let i = 0; i < blogs.length; i++) {
+    let blog = blogs[i];
+
+    const section = document.createElement("section");
+    blogList.appendChild(section);
+
+    const h3 = document.createElement("h3");
+    h3.textContent = blog.title;
+
+    const p = document.createElement("p");
+    p.textContent = blog.ventText;
+
+    const footer = document.createElement("footer");
+    footer.textContent = "Made with 💜 by " + blog.username;
+
+    section.appendChild(h3);
+    section.appendChild(p);
+    section.appendChild(footer);
+  }
+}
+
+function init() {
+  const storedBlogs = JSON.parse(localStorage.getItem("blog"));
+
+  if (storedBlogs !== null) {
+    blogs = storedBlogs;
+  }
+
+  renderBlogs();
+}
+
+init();
--- assets/js/form.js
+++ assets/js/form.js
@@ -3,16 +3,33 @@ const usernameEl = document.querySelector("#username");
 const titleEl = document.querySelector("#title");
 const ventTextEl = document.querySelector("#vent-text");
 const submitBtn = document.querySelector("#submit");
+const problemEl = document.querySelector("#problem");
+let blogPostList = JSON.parse(localStorage.getItem("blog")) || [];
+
 //Event Listener
 submitBtn.addEventListener("click", function (event) {
   //Preventing Default Behavior
   event.preventDefault();
-  //Creating Vent Post Object
-  const ventPost = {
-    username: usernameEl.value,
-    title: titleEl.value,
-    ventText: ventTextEl.value,
-  };
-  //Storing Data in Local Storage
-  localStorage.setItem("blog", JSON.stringify(ventPost));
+
+  if (
+    usernameEl.value === "" ||
+    titleEl.value === "" ||
+    ventTextEl.value === ""
+  ) {
+    problemEl.textContent =
+      "Please make sure to enter your username, title of the post, and of course your vent post, please write to your heart's content.";
+  } else {
+    //Creating Vent Post Object
+    const ventPost = {
+      username: usernameEl.value,
+      title: titleEl.value,
+      ventText: ventTextEl.value,
+    };
+
+    blogPostList.push(ventPost);
+    //Storing Data in Local Storage
+    localStorage.setItem("blog", JSON.stringify(blogPostList));
+
+    window.location.replace("blog.html");
+  }
 });
